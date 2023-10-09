ca070b21bc3ac64ff93765a6a19659a56b7d5d3a
Dex <kevin.decker.bn@gmail.com>
2023-10-09 16:39:46 +0100

    Added mongoose setup

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-const myVariable: string = 'Hello World';
-const myNumber: number = 8;
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+// load env variables so they are available under process.env
+dotenv.config({ path: `./config/.env.${process.env.NODE_ENV}` });
+
+/** Connect to Mongo */
+mongoose
+    .connect(`${process.env.DATABASE_URL}`, { retryWrites: true, w: 'majority' })
+    .then(() => {
+        console.log('Mongo connected successfully.');
+    })
+    .catch((error) => {
+        console.log(error)
+    });
+
